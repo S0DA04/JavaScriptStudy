06abd91af49081ad1586ad944811c9bba51e7adb
S0DA04 <howling0624@naver.com>
2022-01-31 23:40:10 +0900

    study

---
+++ Script.js
@@ -0,0 +1,6 @@
+//console.log("");  콘솔에 내부내용 출력  - 문자열 문자 숫자 전부 가능
+//const a = " ";  값 변경 불가
+//let a = " ";  값 변경 가능
+//Boolean: const a = true; 이렇게 씀 - null: 아무것도 없음 / undefined: 정의 되지 않음
+//const array = [1, "a", ...] ; / array.push(...); 제일 뒤에 값 추가
+//const player{name: "name"};
