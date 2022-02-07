b94235331aaf0f5e39e0f402b5a44ecaba5235a8
S0DA04 <howling0624@naver.com>
2022-02-08 00:08:46 +0900

    studying how to get elements

--- Script.js
+++ Script.js
@@ -1,10 +1,26 @@
-//console.log("");  콘솔에 내부내용 출력  - 문자열 문자 숫자 전부 가능
-//const a = " ";  값 변경 불가
-//let a = " ";  값 변경 가능
-//Boolean: const a = true; 이렇게 씀 - null: 아무것도 없음 / undefined: 정의 되지 않음
-//const array = [1, "a", ...] ; / array.push(...); 제일 뒤에 값 추가
-//const player{name: "name"};
-//function sayhello (){
-//    console.log("Hello")
-//}
+// Study - Line
+// console.log("");  콘솔에 내부내용 출력  - 문자열 문자 숫자 전부 가능
+// const a = " ";  값 변경 불가
+// let a = " ";  값 변경 가능
+// Boolean: const a = true; 이렇게 씀 - null: 아무것도 없음 / undefined: 정의 되지 않음
+// const array = [1, "a", ...] ; / array.push(...); 제일 뒤에 값 추가
+// const player{name: "name"};
+// function sayhello (){
+//     console.log("Hello")
+// }
+// parseInt ("string")
+// const title = document.getElementById("")
+// const hellos = document.getElementsByClassName("title") - <h1 class = "title"> Grab me! </h1>
+// const title =document.getElementsByTagName("h1"); - <div class = "title"> <h1>Grab me!</h1> </div>
+// const title = document.querySelector(".title h1"); - <div class = "title"> <h1>Grab me!</h1> </div> -- id 의 경우 hello 앞에 #
+
+// ____________________________________________________________________________________________________________
+// ____________________________________________________________________________________________________________
+
+
+
+
+
+
+
 
